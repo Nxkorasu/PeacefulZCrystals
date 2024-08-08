689c368037586d4eb6786e445bb366f6538d71fb
Nicolas <clashofnico5903@gmail.com>
2024-08-08 11:07:05 +0200

    changed to only blocks

--- src/main/java/dev/huli/zcrystals/items/serverside/BlankZCrystal.java
+++ src/main/java/dev/huli/zcrystals/items/serverside/BlankZCrystal.java
@@ -1,6 +1,6 @@
 package dev.huli.zcrystals.items.serverside;
 
-import dev.huli.zcrystals.util.ZCrystalsClientSide;
+import dev.huli.zcrystals.util.ZCrystalsServerSide;
 import dev.huli.zcrystals.util.ZCrystalsServerSide;
 import eu.pb4.polymer.core.api.item.SimplePolymerItem;
 import eu.pb4.polymer.resourcepack.api.PolymerModelData;
@@ -80,7 +80,7 @@ public class BlankZCrystal extends SimplePolymerItem {
             player.giveItemStack(new ItemStack(ZCrystalsServerSide.GROUNDIUM_Z));
             return ActionResult.SUCCESS;
         }
-        if(player.getWorld().getDifficulty() == Difficulty.PEACEFUL && block == Blocks.POTATOES){
+        if(block == Blocks.POTATOES){
             heldStack.decrement(1);
             player.giveItemStack(new ItemStack(ZCrystalsServerSide.POISONIUM_Z));
             return ActionResult.SUCCESS;
@@ -105,39 +105,23 @@ public class BlankZCrystal extends SimplePolymerItem {
             player.giveItemStack(new ItemStack(ZCrystalsServerSide.PSYCHIUM_Z));
             return ActionResult.SUCCESS;
         }
+        if(block == Blocks.SHULKER_BOX){
+            heldStack.decrement(1);
+            player.giveItemStack(new ItemStack(ZCrystalsServerSide.FLYNIUM_Z));
+            return ActionResult.SUCCESS;
+        }
+        if(block == Blocks.CAKE_BLOCK){
+            heldStack.decrement(1);
+            player.giveItemStack(new ItemStack(ZCrystalsServerSide.FAIRIUM_Z));
+            return ActionResult.SUCCESS;
+        }
+        if(block == Blocks.IRON_BLOCK){
+            heldStack.decrement(1);
+            player.giveItemStack(new ItemStack(ZCrystalsServerSide.STEELIUM_Z));
+            return ActionResult.SUCCESS;
+        }
         return ActionResult.PASS;
     }
-    @Override
-    public ActionResult useOnEntity(ItemStack itemStack, PlayerEntity playerEntity, LivingEntity livingEntity, Hand hand) {
-
-        if(livingEntity instanceof ChickenEntity){
-            itemStack.decrement(1);
-            livingEntity.kill();
-            playerEntity.giveItemStack(new ItemStack(ZCrystalsClientSide.FLYINIUM_Z));
-            return ActionResult.CONSUME;
-        }
-        if(livingEntity instanceof AllayEntity){
-            itemStack.decrement(1);
-            playerEntity.giveItemStack(new ItemStack(ZCrystalsClientSide.FAIRIUM_Z));
-            return ActionResult.CONSUME;
-        }
-        if(livingEntity instanceof IronGolemEntity){
-            itemStack.decrement(1);
-            livingEntity.kill();
-            playerEntity.giveItemStack(new ItemStack(ZCrystalsClientSide.STEELIUM_Z));
-            return ActionResult.CONSUME;
-        }
-        if(playerEntity.getEntityWorld().getDifficulty() != Difficulty.PEACEFUL && livingEntity instanceof CaveSpiderEntity){
-            itemStack.decrement(1);
-            livingEntity.kill();
-            playerEntity.giveItemStack(new ItemStack(ZCrystalsClientSide.POISONIUM_Z));
-            return ActionResult.CONSUME;
-        }
-
-        return ActionResult.PASS;
-    }
-
-
     @Override
     public Item getPolymerItem(ItemStack itemStack, @Nullable ServerPlayerEntity player){
         return super.getPolymerItem(itemStack,player);
